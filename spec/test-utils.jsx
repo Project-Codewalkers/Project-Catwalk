cd520863011b5699fa195608bf31825172e4d7b4
Luke Henry <luke82601@lukes-air.home>
2021-04-18 07:33:20 -0600

    Added basic RTL framework and test example

--- spec/test-utils.jsx
+++ spec/test-utils.jsx
@@ -1,2 +1,12 @@
+// eslint-disable-no-undef
+import '@testing-library/jest-dom';
 import React from 'react';
 import { render, fireEvent, waitFor, screen } from '@testing-library/react';
+import App from '../client/src/App';
+
+describe('App', () => {
+  test('it should render Hello World', () => {
+    render(<App />);
+    expect(document.getElementById('root').toBeInTheDocument());
+  });
+});
