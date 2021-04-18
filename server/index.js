a21313de6e0401cf9a2710e10c124469b02b2c8b
Brenton Hershner <brenthershner@gmail.com>
2021-04-18 14:41:57 -0700

    setup proxy server

--- server/index.js
+++ server/index.js
@@ -1,24 +1,25 @@
 const express = require('express');
 const path = require('path');
 
+const morgan = require('morgan');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const Authentication = require('./config.js');
+// TODO: add cors
+
 const app = express();
 const PORT = 3000;
-const { getAllProducts } = require('../helpers/atelier');
-
+const baseURL = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-sea/';
 const fullPath = path.join(__dirname, '/../client/dist');
-// console.log(fullPath);
+
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.static(fullPath));
 
-app.get('/products', (req, res) => {
-  getAllProducts()
-    .then((response) => {
-      res.status(201).send(response.data);
-    })
-    .catch((error) => {
-      res.status(400).send(error);
-    });
-});
+app.use('/(\\w)+', createProxyMiddleware({
+  target: baseURL,
+  changeOrigin: true,
+  headers: Authentication,
+}));
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
