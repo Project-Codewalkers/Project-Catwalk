61d57843a39b58ee0d1002b612f1c361cfb83786
Luke Henry <luke82601@lukes-air.home>
2021-04-18 08:35:53 -0600

    Added RTL render test for App component

--- spec/test-utils.jsx
+++ spec/test-utils.jsx
@@ -1,2 +1,12 @@
+/* eslint-disable no-undef */
+import '@testing-library/jest-dom';
+import { render } from '@testing-library/react';
 import React from 'react';
-import { render, fireEvent, waitFor, screen } from '@testing-library/react';
+import App from '../client/src/App';
+
+describe('App', () => {
+  test('App should render', () => {
+    render(<App />);
+    expect(document.getElementById('root')).toBeInTheDocument();
+  });
+});
