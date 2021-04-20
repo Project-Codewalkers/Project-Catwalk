70f5a754bc54e5321fcf7efcd2eb3d7959d5bed9
Luke Henry <luke82601@lukes-air.home>
2021-04-20 17:56:40 -0600

    Related Items renders dummy items

--- client/src/Components/RelatedItems/OneItem.jsx
+++ client/src/Components/RelatedItems/OneItem.jsx
@@ -1,7 +1,25 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
+// import PropTypes from 'prop-types';
 
 const OneItem = ({ item }) => (
-  <li><img src={item.image}/></li>
-)
+  <li>
+    {item.id}
+    <br />
+    {item.category}
+    <br />
+    {item.product_name}
+    <br />
+    {item.price}
+    <br />
+    {item.stars}
+    <br />
+    <img src={item.image} alt="related-items-list" />
+  </li>
+);
+
+// OneItem.propTypes = {
+//   image: PropTypes.string.isRequired,
+// };
 
 export default OneItem;
--- client/src/Components/RelatedItems/RelatedItems.jsx
+++ client/src/Components/RelatedItems/RelatedItems.jsx
@@ -1,14 +1,41 @@
 import React from 'react';
 // import sampleData from '../../sampleData';
+import PropTypes from 'prop-types';
 import OneItem from './OneItem';
 
-const RelatedItems = ({ sample }) => (
+const sampleData = [
+  {
+    id: 1,
+    category: 'A thing like the thing above',
+    product_name: 'Thing 1',
+    price: '$500.00',
+    stars: 5,
+    image: 'https://tse1.explicit.bing.net/th?id=OIP.Xyr9Zw5XAPMmiamYfxaelwHaE8&pid=Api&P=0&w=278&h=186',
+  },
+  {
+    id: 2,
+    category: 'A thing like the thing above',
+    product_name: 'Thing 2',
+    price: '$450.00',
+    stars: 3,
+    image: 'https://tse1.mm.bing.net/th?id=OIP.5JOmFuIk6NBgsbjVQYFNOQHaE8&pid=Api&P=0&w=278&h=186',
+  },
+];
+
+const RelatedItems = () => (
+
   <div>
-    Related Items
-    {sample.map((item, i) => (
-      <OneItem key={item + i} item={item} />
+    <h1>
+      Related Items
+    </h1>
+    {sampleData.map((item) => (
+      <OneItem key={item.id} item={item} />
     ))}
   </div>
 );
+
+sampleData.propTypes = {
+  image: PropTypes.string.isRequired,
+};
 
 export default RelatedItems;
