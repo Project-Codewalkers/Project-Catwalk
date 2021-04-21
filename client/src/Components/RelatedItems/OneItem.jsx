51473ea06cb5dc39c700024dbf8020738f49e5e7
Luke Henry <luke82601@lukes-air.home>
2021-04-21 14:01:37 -0600

    Added CSS thru styled-components

--- client/src/Components/RelatedItems/OneItem.jsx
+++ client/src/Components/RelatedItems/OneItem.jsx
@@ -1,10 +1,18 @@
-/* eslint-disable react/prop-types */
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const Carousel = styled.div`
+  padding: 10px;
+  margin: 10px;
+  background-color: #decddd;
+  float: left;
+  border: black solid 0.1em;
+`;
 
 const OneItem = ({ item }) => (
-  <li>
-    {item.id}
+  <Carousel>
+    <img src={item.image} alt="related-items-list" />
     <br />
     {item.category}
     <br />
@@ -14,8 +22,8 @@ const OneItem = ({ item }) => (
     <br />
     {item.stars}
     <br />
-    <img src={item.image} alt="related-items-list" />
-  </li>
+    <br />
+  </Carousel>
 );
 
 // OneItem.propTypes = {
--- client/src/Components/RelatedItems/RelatedItems.jsx
+++ client/src/Components/RelatedItems/RelatedItems.jsx
@@ -1,37 +1,39 @@
 import React from 'react';
 // import sampleData from '../../sampleData';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 import OneItem from './OneItem';
 
 const sampleData = [
   {
     id: 1,
-    category: 'A thing like the thing above',
-    product_name: 'Thing 1',
+    category: 'Category',
+    product_name: 'Product Name 1',
     price: '$500.00',
     stars: 5,
     image: 'https://tse1.explicit.bing.net/th?id=OIP.Xyr9Zw5XAPMmiamYfxaelwHaE8&pid=Api&P=0&w=278&h=186',
   },
   {
     id: 2,
-    category: 'A thing like the thing above',
-    product_name: 'Thing 2',
+    category: 'Category',
+    product_name: 'Product Name 2',
     price: '$450.00',
     stars: 3,
     image: 'https://tse1.mm.bing.net/th?id=OIP.5JOmFuIk6NBgsbjVQYFNOQHaE8&pid=Api&P=0&w=278&h=186',
   },
 ];
 
-const RelatedItems = () => (
+const Wrapper = styled.div`
 
-  <div>
-    <h1>
-      Related Items
-    </h1>
+`;
+
+const RelatedItems = () => (
+  <Wrapper>
+    <h1>Related Items</h1>
     {sampleData.map((item) => (
       <OneItem key={item.id} item={item} />
     ))}
-  </div>
+  </Wrapper>
 );
 
 sampleData.propTypes = {
