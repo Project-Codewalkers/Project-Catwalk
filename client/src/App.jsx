23a2e5484f4e526dff34229bc63bc8edd8adcf6f
Brenton Hershner <brenthershner@gmail.com>
2021-04-23 09:43:04 -0700

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@
 import React, { useState, useEffect } from 'react';
 import ProductDetail from './Components/ProductDetail/ProductDetail';
 import Carousel from './Components/RelatedItems/Carousel';
-import RelatedItems from './Components/RelatedItems/RelatedItems';
+// import RelatedItems from './Components/RelatedItems/RelatedItems';
 import ReviewList from './Components/Review/ReviewList';
 import api from './lib/api';
 
@@ -16,7 +16,7 @@ const [id, setId] = useState(20100);
       <p>{console.log(id)}</p>
       <ProductDetail productId={id} setProductId={setId} />
       <Carousel productId={productId} />
-      <RelatedItems setProduct={setProductId} />  
+//       <RelatedItems setProduct={setProductId} />  
       <ReviewList id={id} setId={setId} />
     </div>
   );
