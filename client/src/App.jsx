039a95ad1ee2209ede50754a69ef1cee5e27b859
Brenton Hershner <brenthershner@gmail.com>
2021-04-23 10:19:40 -0700

    fixed some bugs in app and carousel

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 // App.jsx
 
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import ProductDetail from './Components/ProductDetail/ProductDetail';
 import Carousel from './Components/RelatedItems/Carousel';
 // import RelatedItems from './Components/RelatedItems/RelatedItems';
@@ -8,17 +8,13 @@ import ReviewList from './Components/Review/ReviewList';
 // import api from './lib/api';
 
 const App = () => {
-  const productId = 20101;
-  const [id, setId] = useState(20100);
+  const [productId, setProductId] = useState(20100);
 
   return (
     <div>
-      Hello World!
-      <p>{console.log(id)}</p>
-      <ProductDetail productId={id} setProductId={setId} />
-      {/* <RelatedItems /> */}
+      <ProductDetail productId={productId} setProductId={setProductId} />
       <Carousel productId={productId} />
-      <ReviewList id={id} setId={setId} />
+      <ReviewList id={productId} />
     </div>
   );
 };
