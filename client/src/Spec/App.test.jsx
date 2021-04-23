03fc5bb70ae551c19dee286a0c634e81eed970e9
Brenton Hershner <brenthershner@gmail.com>
2021-04-22 22:09:37 -0700

    progress. Pushing to see if circleCI still builds it

--- client/src/Spec/App.test.jsx
+++ client/src/Spec/App.test.jsx
@@ -1,27 +1,17 @@
 /* eslint-disable no-undef */
 import '@testing-library/jest-dom';
+import { act } from 'react-dom/test-utils';
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from '../App';
-import { act } from 'react-test-utils';
+// import ReactTestUtils from 'react-test-utils';
 
+global.fetch = jest.fn().mockResolvedValue('fakeData');
 describe('App', () => {
-  global.fetch = jest.fn().mockResolvedValue('fakeData');
-  let container;
-
-  beforeEach(() => {
-    container = document.createElement('div');
-    document.body.appendChild(container);
-  });
-
-  afterEach(() => {
-    document.body.removeChild(container);
-    container = null;
-  });
-
-  act(() => {
-    ReactDOM.render(<App />, container)
-  });
   test('it should render the App component', () => {
+    // act(() => { render(<App />); });
+    // render(<App />);
+    // screen.debug();
+    // expect(screen.getByText()).toBeInTheDocument();
   });
 });
