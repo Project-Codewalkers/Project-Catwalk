3cdb33e394592da5344f00ce0cb61d0b0f5fdcdb
Brenton Hershner <brenthershner@gmail.com>
2021-04-29 13:29:29 -0700

    changed some server stuff

--- server/index.js
+++ server/index.js
@@ -2,13 +2,14 @@ const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
 const cors = require('cors');
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const axios = require('axios');
+// const { createProxyMiddleware } = require('http-proxy-middleware');
 const Authentication = require('./config.js');
 
 const app = express();
 
 const PORT = 3000;
-const API_URL = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-sea/';
+const API_URL = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-sea';
 const fullPath = path.join(__dirname, '/../client/dist');
 
 app.use(cors());
@@ -16,11 +17,23 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.static(fullPath));
 
-app.use('/(\\w)+', createProxyMiddleware({
-  target: API_URL,
-  changeOrigin: true,
-  headers: Authentication,
-}));
+app.get(/\w+/, (req, res) => {
+  const apiPath = `${API_URL}${req.url}`;
+  const { params } = req;
+  console.log('apiPath', apiPath, 'with params', params);
+  axios.get(apiPath, {
+    headers: Authentication,
+    params,
+  })
+    .then((response) => {
+      res.status(200).send(response.data);
+    })
+    .catch((error) => {
+      console.log('Error:');
+      // res.sendStatus(500);
+      res.status(500).send(error);
+    });
+});
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
