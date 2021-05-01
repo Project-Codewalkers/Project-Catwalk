09f0abddd9ce35b0739fa9ab7a72905ea86fc21b
Brenton Hershner <brenthershner@gmail.com>
2021-04-30 22:14:44 -0700

    added conditionals on window so it doesn't fail tests

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import ProductDetail from './Components/ProductDetail/ProductDetail';
 import Carousel from './Components/RelatedItems/Carousel';
 // import RelatedItems from './Components/RelatedItems/RelatedItems';
 import ReviewList from './Components/Review/ReviewList';
-import { avgStars, totalReviews } from './Components/RelatedItems/Stars';
+import { avgStars } from './Components/RelatedItems/Stars';
 import api from './lib/api';
 import Modal from './Components/Review/Modal';
 
